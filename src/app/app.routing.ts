5442446826b7291de42273fecda5219dac2e8dbe
adtm <tomas.eglinskas@gmail.com>
2017-06-22 20:25:25 +0300

    feat: added 404 error page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { PortalComponent } from './portal/portal.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const appRoutes: Routes = [
   { path: '', component: AuthenticationComponent },
-  { path: 'main', component: PortalComponent}
+  { path: 'main', component: PortalComponent },
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '/404' }
 ];
