e27fd28d78750c667636202873c2cb037b59a7af
Gashanas <gashanas@gmail.com>
2017-06-22 16:13:32 +0300

    created card

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -5,12 +5,18 @@ import { RouterModule } from '@angular/router';
 import { PortalComponent } from './portal.component';
 import { appRoutes } from './portal.routing';
 import { NavbarModule } from '../shared/navbar/navbar.module';
+import { MdGridListModule, MdButtonModule, MdCardModule, MdInputModule, MdListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forChild(appRoutes),
-    NavbarModule
+    NavbarModule,
+    MdInputModule,
+    MdCardModule,
+    MdButtonModule,
+    MdGridListModule,
+      MdListModule,
   ],
   exports: [],
   declarations: [
